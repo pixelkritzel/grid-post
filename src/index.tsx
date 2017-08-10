e13962b65ce0e6833f77f81ba0c3019af693c8bf
Timo Zoeller <tzoe@unitb-technology.de>
2017-08-10 21:09:06 +0200

    Prevents dropping files on the wrong place and electron loading them

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 
+document.addEventListener('dragover', event => event.preventDefault());
+document.addEventListener('drop', event => event.preventDefault());
+
 const DropApp = DragDropContext(HTML5Backend)(App);
 
 ReactDOM.render(<DropApp />, document.getElementById('root'));
