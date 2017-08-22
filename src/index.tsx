96206d8cf1b3953be443a4db10eda48ad3f3a2ed
Timo Zoeller <timo@pixelkritzel.de>
2017-08-22 20:04:40 +0200

    Remove fragments of react-dnd

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContext } from 'react-dnd';
 
 document.addEventListener('dragover', event => event.preventDefault());
 document.addEventListener('drop', event => event.preventDefault());
 
-const DropApp = DragDropContext(HTML5Backend)(App);
-
-ReactDOM.render(<DropApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
