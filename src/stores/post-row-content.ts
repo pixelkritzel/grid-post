8d53723c21433f61c1f0028aa95e69a1875b11e5
Timo Zoeller <timo@pixelkritzel.de>
2017-08-31 15:56:03 +0200

    Updates code to mobx-state-tree 0.11

--- src/stores/post-row-content.ts
+++ src/stores/post-row-content.ts
@@ -1,12 +1,9 @@
 import { types, getParent } from 'mobx-state-tree';
-
 import resourceModel from './resource-model';
-
 import { PostRowColumnModelType } from './post-row-column';
-
 type DirectionsType = 'top' | 'right' | 'bottom' | 'left';
-export const PostRowContentModel = types.model(
-  {
+export const PostRowContentModel = types
+  .model({
     cid: types.identifier(),
     resource: types.reference(resourceModel),
     height: 100,
@@ -14,33 +11,32 @@ export const PostRowContentModel = types.model(
     marginRight: '0px',
     marginBottom: '0px',
     marginLeft: '0px'
-  },
-  {
-    setHeight(height: number) {
-      const contents: PostRowContentModelType[] = getParent(this, 1);
-      const indexOfThis = contents.indexOf(this);
-      this.height = height;
+  })
+  .actions(self => {
+    function setHeight(height: number) {
+      const contents: (typeof self)[] = getParent(self, 1);
+      const indexOfThis = contents.indexOf(self);
+      self.height = height;
       if (contents.length > 1) {
         const otherContentsHeight = 100 - height;
         const otherContent = contents[indexOfThis + 1];
         otherContent.height = otherContentsHeight;
       }
-    },
-    setMargin(direction: DirectionsType, value: string) {
-      const key = 'margin' + direction.charAt(0).toUpperCase() + direction.slice(1);
-      this[key] = value;
-    },
-    remove() {
-      const parentColumn: PostRowColumnModelType = getParent(this, 2);
-      const indexOfThis = parentColumn.contents.indexOf(this);
-      parentColumn.contents.splice(indexOfThis, 1);
-      if (parentColumn.contents.length === 0) {
-        parentColumn.remove();
-      } else if (parentColumn.contents.length === 1) {
-        parentColumn.contents[0].setHeight(100);
-      }
     }
-  }
-);
-
+    function setMargin(direction: DirectionsType, value: string) {
+      const key = 'margin' + direction.charAt(0).toUpperCase() + direction.slice(1);
+      self[key] = value;
+    }
+    function remove() {
+      const parentColumn: PostRowColumnModelType = getParent(self, 2);
+      const contents: (typeof self)[] = parentColumn.contents;
+      const indexOfThis = contents.indexOf(self);
+      parentColumn.removeContent(indexOfThis);
+    }
+    return {
+      setHeight,
+      setMargin,
+      remove
+    };
+  });
 export type PostRowContentModelType = typeof PostRowContentModel.Type;
